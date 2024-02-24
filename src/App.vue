6aa127d0ceb73989c926809133adf6125f1fa633
ballin-98 <alexbali189@gmail.com>
2024-02-24 10:13:22 -0700

    updated some stuff

--- src/App.vue
+++ src/App.vue
@@ -8,6 +8,7 @@ import TheWelcome from './components/TheWelcome.vue'
     <img alt="Vue logo" class="logo" src="./assets/logo.svg" width="125" height="125" />
 
     <div class="wrapper">
+      <div class="something">another class that hopefully shows up</div>
       <HelloWorld msg="You did it!" />
     </div>
   </header>
