fdea1f60d5baf318192c550468c6f66dceea9c00
ballin-98 <alexbali189@gmail.com>
2024-08-04 22:32:08 -0600

    added scroll composable

---
+++ src/composables/scroll.ts
@@ -0,0 +1,20 @@
+import { screenSizeHelper } from '@/composables/isMobile'
+
+const scrollToTag = (tagName: string): void => {
+  const elementToScrollTo = document.querySelector(tagName)
+
+  if (elementToScrollTo) {
+    elementToScrollTo.scrollIntoView({ behavior: 'smooth' })
+  } else {
+    console.log(`there was an error scrolling to element: ${tagName}}`)
+  }
+}
+
+export const scrollToProjects = () => {
+  const { isMobile } = screenSizeHelper()
+  if (isMobile) {
+    scrollToTag('#projects-mobile')
+  } else {
+    scrollToTag('#projects')
+  }
+}
