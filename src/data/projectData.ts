11498d43c1fc058a0d0ab22714925e408f141113
ballin-98 <alexbali189@gmail.com>
2024-08-16 10:10:31 -0600

    updated image path

--- src/data/projectData.ts
+++ src/data/projectData.ts
@@ -17,7 +17,7 @@ export const projectList: projectCardData[] = [
     title: 'LawDepot eSign',
     description:
       'An electronic signature web application, built with Vue 3 and .NET Core, enabling users to seamlessly share and collaborate on documents.',
-    image: '/portfolio/assets/eSign.png',
+    image: '/assets/eSign.png',
     tags: [
       { title: 'Vue 3', showTag: true },
       { title: '.Net 6', showTag: true },
@@ -38,7 +38,7 @@ export const projectList: projectCardData[] = [
     title: 'LawDepot Advanced Editor',
     description:
       'A document editing application, built with Vue 3 and .NET Core, that empowers users to edit, share, and collaborate on legal documents. The application also supports importing Word documents and PDFs converting them to an editable format',
-    image: '/portfolio/assets/ae.png',
+    image: '/assets/ae.png',
     tags: [
       { title: 'Vue 3', showTag: true },
       { title: '.Net 6', showTag: true },
@@ -59,7 +59,7 @@ export const projectList: projectCardData[] = [
     title: 'Stress Testing Application',
     description:
       'Developed with Python and the Locust framework, this application performs stress testing on endpoints, offering valuable insights into potential vulnerabilities.',
-    image: '/portfolio/assets/stress_testing.jpg',
+    image: '/assets/stress_testing.jpg',
     tags: [
       { title: 'Python', showTag: true },
       { title: 'Locust', showTag: true },
@@ -71,7 +71,7 @@ export const projectList: projectCardData[] = [
     title: 'Dividend Calculator',
     description:
       'A light weight web application built with Vue 3, designed to calculate and analyze returns on investments for dividend-focused investors.',
-    image: '/portfolio/assets/dividend_calculator.jpg',
+    image: '/assets/dividend_calculator.jpg',
     tags: [
       { title: '.Net 6', showTag: true },
       { title: 'JavaSript', showTag: true },
@@ -83,7 +83,7 @@ export const projectList: projectCardData[] = [
     title: 'Citrus Network',
     description:
       'A social media website dedicated built using Django and React, dedicated to keeping information private while letting you connect with friends',
-    image: '/portfolio/assets/CitrusNetwork.jpg',
+    image: '/assets/CitrusNetwork.jpg',
     tags: [
       { title: 'React', showTag: true },
       { title: 'TypeScript', showTag: true },
@@ -97,7 +97,7 @@ export const projectList: projectCardData[] = [
     title: 'ETL - City of Edmonton',
     description:
       'An automated workflow developed with FME that extracts, transforms, and aggregates data from more than 10 diverse sources, which is then used in data visualizations to provide actionable insights',
-    image: '/portfolio/assets/etl.jpg',
+    image: '/assets/etl.jpg',
     tags: [
       { title: 'FME', showTag: true },
       { title: 'Python', showTag: true },
@@ -110,7 +110,7 @@ export const projectList: projectCardData[] = [
     title: 'Property Scroller',
     description:
       'A project built with Python and Selenium that gathers housing data from different sources and stores it',
-    image: '/portfolio/assets/house.jpg',
+    image: '/assets/house.jpg',
     tags: [
       { title: 'Selenium', showTag: true },
       { title: 'Python', showTag: true },
